b97e1040c2d4cc50c9978b2557cb97302c0336f2
LightslicerGP <Lightslicerandpuzzlegamer@gmail.com>
2022-12-18 15:54:09 -0500

    minor tweaks

--- DailyBoard/DailyBoard.js
+++ DailyBoard/DailyBoard.js
@@ -7,7 +7,7 @@ setInterval(() => {
 		hh = day.getHours() * 30,
 		mm = day.getMinutes() * deg,
 		ss = day.getSeconds() * deg;
-	hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+	hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
 	mn.style.transform = `rotateZ(${mm}deg)`;
 	sc.style.transform = `rotateZ(${ss}deg)`;
 })
