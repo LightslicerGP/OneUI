2da2d57820c989d8508ff902c5dd5d91050ef294
LightslicerGP <Lightslicerandpuzzlegamer@gmail.com>
2024-01-08 11:55:07 -0500

    Used PWABuilder Website
    1/8/24 used pwabuilder website to buff manifest and stuff

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,33 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
+);
 
 workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'image',
-    new workbox.strategies.NetworkFirst() //CacheFirst() will go to cache saved first instead of the website
+  ({ request }) => request.destination === "image",
+  new workbox.strategies.NetworkFirst() //CacheFirst() will go to cache saved first instead of the website
 );
+
+if ("serviceWorker" in navigator && "SyncManager" in window) {
+  // Get our service worker registration.
+  const registration = await navigator.serviceWorker.registration;
+
+  try {
+    // This is where we request our sync.
+    // We give it a "tag" to allow for differing sync behavior.
+    await registration.sync.register("database-sync");
+  } catch {
+    console.log("Background Sync failed.");
+  }
+}
+
+// Add an event listener for the `sync` event in your service worker.
+self.addEventListener("sync", (event) => {
+  // Check for correct tag on the sync event.
+  if (event.tag === "database-sync") {
+    // Execute the desired behavior with waitUntil().
+    event.waitUntil(
+      // This is just a hypothetical function for the behavior we desire.
+      pushLocalDataToDatabase()
+    );
+  }
+}); // 1/8/24
